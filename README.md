6404490f5f9fb10cfed642816dc4a3780cd84f21
梁马勇 <liangmayong@qutoutiao.net>
2019-08-01 18:03:57 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# str-webpack-plugin
+> str-webpack-plugin
+
+## Getting started
+* make sure node and npm installed;
+
+### Install
+```
+> npm install str-webpack-plugin --save
+```
+
+### Usage
+#####webpack.config.js
+```javascript
+//webpack.config.js
+var path = require('path');
+var strPlugin = require('str-webpack-plugin');
+
+module.exports = {
+  ...
+  plugins: [
+    new strPlugin({
+      src: path.resolve('./dist'),
+      test: /\.js/g,
+      replace: [{
+          form: ["foo"],
+          to: ["bar"],
+      }]
+    })
+  ]
+  ...
+}
+```
---
+++ index.js
@@ -0,0 +1,93 @@
+const fs = require("fs");
+const path = require("path");
+
+function scan(src, regex) {
+    let result = [];
+    const _scan = (next) => {
+        if (!next) {
+            return;
+        }
+        const files = fs.readdirSync(next);
+        if (!files.length) {
+            return;
+        }
+        files.forEach(file => {
+            let absFile = path.join(next, `./${file}`);
+            let fileStat = fs.statSync(absFile);
+            if (fileStat.isDirectory()) {
+                return _scan(absFile);
+            }
+            if (fileStat.isFile() && (!regex || regex.test(file.toLowerCase()))) {
+                result.push(absFile);
+            }
+        });
+    }
+    if (src instanceof Array) {
+        src.forEach(dir => {
+            _scan(dir);
+        });
+    } else {
+        _scan(src);
+    }
+    return result;
+}
+
+function replace(content, options) {
+    if (!options) {
+        return content;
+    }
+    if (options instanceof Array) {
+        options.forEach(option => {
+            content = replace(content, option);
+        });
+    } else if (options.form) {
+        if (options.form instanceof Array) {
+            options.form.forEach((form, index) => {
+                content = replace(content, { form, to: options.to[index] });
+            });
+        } else {
+            let regex = new RegExp(options.form, 'g');
+            content = content.replace(regex, options.to || '');
+        }
+    }
+    return content;
+}
+
+function handle(options = {}) {
+    if (options instanceof Array) {
+        options.forEach(option => {
+            handle(option);
+        });
+    } else if (options.src) {
+        let files = scan(options.src, options.test);
+        files.forEach(file => {
+            if (options.debug) {
+                console.log('str-webpack-plugin >>> ', file);
+            }
+            let content = fs.readFileSync(file, 'utf8');
+            content = replace(content, options.replace);
+            fs.writeFileSync(`${file}`, content);
+        });
+        if (options.debug) {
+            console.log('');
+        }
+    }
+}
+
+function pligin(options) {
+    this.options = options;
+}
+
+pligin.prototype.apply = function (compiler) {
+    if (compiler.hooks) {
+        compiler.hooks.afterEmit.tap("done", () => {
+            handle(this.options);
+        })
+    } else {
+        compiler.plugin('done', function () {
+            handle(this.options);
+        });
+    }
+}
+
+module.exports = exports = pligin
